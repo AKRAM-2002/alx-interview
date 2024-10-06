9cfd50a478f92e652e9bfef5bae14055231a05cb
AKRAM-2002 <Akrambusiness6@gmail.com>
2024-10-06 07:24:56 +0100

    done

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 """
-
+0. Pascal's Triangle
 """
 def pascal_triangle(n):
-    list_of_lists = []
+    """
+    Returns a list of lists representing the first N rows of Pascal's Triangle.
+    """
+    
     if n <= 0:
-        return list_of_lists
+        return []
+    
+    triangle = []
         
     for i in range(n):
         row = [1]  # Every row starts with 1
         if i > 0:
-            last_row = list_of_lists[i - 1]
+            last_row = triangle[i - 1]
             row.extend([sum(pair) for pair in zip(last_row, last_row[1:])])
             row.append(1)  # Only append 1 if it's not the first row
-        list_of_lists.append(row)
+        triangle.append(row)
     
-    return list_of_lists
+    return triangle
