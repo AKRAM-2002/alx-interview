da6fbd0dcca4a71a3eecbd8cf08a0154a109733d
AKRAM-2002 <Akrambusiness6@gmail.com>
2024-10-14 06:41:00 +0100

    pycodestyle fixe

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,22 +4,23 @@ Lockboxes problem
 """
 from typing import List
 
+
 def canUnlockAll(boxes: List[List[int]]) -> bool:
     """
-    A method that determines if all the boxes 
+    A method that determines if all the boxes
     can be opened.
-    Returns True if all boxes can be opened, else 
+    Returns True if all boxes can be opened, else
     returns False.
     """
-    n = len(boxes)  
-    unlocked = set([0])  
-    keys = [0]  
-    
+    n = len(boxes)
+    unlocked = set([0])
+    keys = [0]
+
     while keys:
-        current_key = keys.pop()  
-        for key in boxes[current_key]: 
-            if key not in unlocked and key < n:  
-                unlocked.add(key)  
-                keys.append(key)  
-    
+        current_key = keys.pop()
+        for key in boxes[current_key]:
+            if key not in unlocked and key < n:
+                unlocked.add(key)
+                keys.append(key)
+
     return len(unlocked) == n
