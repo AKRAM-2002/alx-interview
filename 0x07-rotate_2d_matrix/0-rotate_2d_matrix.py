1cc12e60456ce685e5e14b47918cd809d9610858
AKRAM-2002 <Akrambusiness6@gmail.com>
2024-11-22 07:11:21 +0100

    update

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,6 +2,8 @@
 """
 Module 0. Rotate 2D Matrix
 """
+
+
 def rotate_2d_matrix(matrix):
     """
     Rotate 2D Matrix
@@ -10,8 +12,6 @@ def rotate_2d_matrix(matrix):
     for i in range(len(matrix)):
         for j in range(i + 1, len(matrix)):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    
+
     for row in matrix:
         row.reverse()
-
-    
